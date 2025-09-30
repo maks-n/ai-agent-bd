2a019de09af34284dc2d715b47202bc0a687534c
maks-n <71183975+maks-n@users.noreply.github.com>
2025-09-29 21:38:40 -0400

    feat: one shot mvp

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
 def main():
     print("Hello from ai-agent-bd!")
+
+    try:
+        request = sys.argv[1]
+    except Exception as e:
+        print("No request was provided.")
+        sys.exit(1)
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents=request
+    )
+    print(response.text)
+
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
 
 
 if __name__ == "__main__":

6b27bc824933f3abd25b15b48186febab8f33654
maks-n <71183975+maks-n@users.noreply.github.com>
2025-10-05 13:19:55 -0400

    feat: add ability to call a function by an agent

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from google import genai
 from google.genai import types
 
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 
 def main():
@@ -55,9 +55,20 @@ def generate_content(client, messages, verbose):
     if not response.function_calls:
         return response.text
 
+    function_call_responses = []
     for function_call_part in response.function_calls:
-        print(f'Calling function: {function_call_part.name}({function_call_part.args}')
+        function_call_result = call_function(function_call_part, verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response.response
+        ):
+            raise Exception("Error: empty function call result")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        function_call_responses.append(function_call_result.parts[0])
 
+        if not function_call_responses:
+            raise Exception("Error: no function call responses generated")
 
 if __name__ == "__main__":
     main()
